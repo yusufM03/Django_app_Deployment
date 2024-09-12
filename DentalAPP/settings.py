939571a937ff1c06850d04be7520a2dfb22d6e88
yusufM03 <youssefmakhlouf139@gmail.com>
2024-09-12 11:28:03 +0100

    modify allowed Hosts

--- DentalAPP/settings.py
+++ DentalAPP/settings.py
@@ -117,8 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+# Add this line to your settings file
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ensure this is also defined
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- DentalAPP/urls.py
+++ DentalAPP/urls.py
@@ -14,16 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.urls import path
 from MeshV import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('',views.upload_mesh,name='upload_mesh'),
-    path('vis/<path:file_path>/',views.Visualization, name='visualization'),
-    path('segment_mesh/',views.segment_mesh,name='segmentation')
-
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.upload_mesh, name='upload_mesh'),
+    path('vis/<path:file_path>/', views.Visualization, name='visualization'),
+    path('segment_mesh/', views.segment_mesh, name='segmentation'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
